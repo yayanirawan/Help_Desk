617fff34a8b044bb87b9311bcf205741d3dd0d6e
yayanirawan <one.tv1992@gmail.com>
2018-11-03 01:03:41 +0700

    sdasd

--- application/controllers/Tiket_Masuk.php
+++ application/controllers/Tiket_Masuk.php
@@ -1,18 +1,59 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Tiket_Masuk extends Rsbt {
+class Tiket_Masuk extends Rsbt 
+{
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->js(base_url('assets/public/app/bobot.js'));
+		// $this->load->js(base_url('assets/public/app/tabel.js'));
+		$this->load->model('Mtiketmasuk');
+		// $this->per_page = (!$this->input->get('per_page')) ? 20 : $this->input->get('per_page');
+		// $this->page = $this->input->get('page');
+		
+	}
 
 	public function index() 
 	{
 		$this->page_title->push('<i class="fa fa-envelope"></i> TIKET MASUK');
 
+		$this->form_validation->set_rules('id_user', 'id_user', 'trim|required');
+		$this->form_validation->set_rules('id_masuk', 'id_masuk', 'trim|required');
+		$this->form_validation->set_rules('id_kategori', 'id_kategori', 'trim|required');
+		$this->form_validation->set_rules('deskripsi', 'deskripsi', 'trim|required');
+		// $this->form_validation->set_rules('lampiran', 'lampiran', 'trim|required');
+		//$this->form_validation->set_rules('tanggal', 'tanggal', 'trim|required');
+
+
+		if ($this->form_validation->run() == TRUE)
+		{
+			$this->Mtiketmasuk->create();
+
+			redirect(current_url());
+		}
+
 		$this->data = array(
 			'title' => "Tiket - Help Desk IT RS. Bhakti Timah", 
-			'breadcrumbs' => $this->breadcrumbs->show(),
+			'breadcrumb' => $this->breadcrumbs->show(),
 			'page_title' => $this->page_title->show(),
-			
+			'category' => $this->Mtiketmasuk->get_all(),
+			'id_masuk' => $this->Mtiketmasuk->get_no_invoice(),
+			'get' => $this->Mtiketmasuk->get_account($this->ADMIN),
+			'data' => $this->Mtiketmasuk->join(),
 		);
+
 		$this->template->view('Test/Tiket_Masuk/Tiket_Masuk',$this->data);
+	}
+
+	public function get_no_invoice()
+	{
+		$this->Mtiketmasuk->get_no_invoice();
+	}
+
+	public function create()
+	{
+		
+		$this->template->view('Test/Tiket_Masuk/Tiket_Masuk', $this->data);
 	}
 }
--- application/views/Test/Tiket_Masuk/Tiket_Masuk.php
+++ application/views/Test/Tiket_Masuk/Tiket_Masuk.php
@@ -1,8 +1,9 @@
 <div class="panel panel-info">
+  <div class="col-md-8 col-md-offset-2 col-xs-12"><?php echo $this->session->flashdata('alert'); ?></div>
   <div class="panel-heading" style="font-size: 18px"><b>Data Tiket Masuk</b> 
     <div class="pull-right"><a href="#tm-add" data-toggle="modal"><button type="button" class="btn btn-info btn-sm"><i class="fa fa-plus-square">  Tambah</i></button></a></div>
   </div>
-
+<pre><?php print_r($data) ?></pre>
     <div class="panel-body">
         <table id="example1" class="table table-bordered table-striped">
           <thead>
@@ -17,6 +18,7 @@
           </tr>
           </thead>
           <tbody>
+
           <tr>
             <td>1</td>
             <td>US-001</td>
@@ -26,33 +28,7 @@
             <td> 4</td>
             <td><div class="btn btn-sm btn-warning">Proses</div></td>
           </tr>
-          <tr>
-            <td>1</td>
-            <td>US-001</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td><div class="btn btn-sm btn-warning">Proses</div></td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>US-001</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td><div class="btn btn-sm btn-warning">Proses</div></td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>US-001</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td>Sungai Liat</td>
-            <td> 4</td>
-            <td><div class="btn btn-sm btn-warning">Proses</div></td>
-          </tr>
+          
           </tbody>
           <tfoot>
           <tr>
@@ -72,49 +48,55 @@
 <!--Add Modal -->
  <div id="tm-add" class="modal fade" role="dialog">
     <div class="modal-dialog">
-        <form action="tm-add" method="post" method="post" enctype="multipart/form-data">>
-            <div class="panel panel-default">
-                <div class="panel-heading bg-success">
-                    <h8 class="modal-title"><i class="fa fa-plus-square"><b> Entry Tiket</b></i></h8>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+ <form action="<?php echo current_url() ?>" method="post" method="post" enctype="multipart/form-data">> -->
+  <div class="panel panel-default">
+      <div class="panel-heading bg-success">
+          <h8 class="modal-title"><i class="fa fa-plus-square"><b> Entry Tiket</b></i></h8>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+           <input type="hidden" name="id_user" class="form-control" value="<?php echo $get->id_user; ?>">
+      </div>
+
+      <div class="panel-body">
+              <div class="col-sm-12">
+                <div class="form-group">
+                  <label for="id_masuk"><b>ID Tiket</b></label>
+                  <input name="id_masuk" type="text" class="form-control" value="<?php echo $id_masuk; ?>" readonly>
                 </div>
-                <div class="panel-body">
-                        <div class="col-sm-12">
-                          <div class="form-group">
-                            <label for="id_tiket"><b>ID Tiket</b></label>
-                            <input name="id_tiket" type="text" class="form-control" value="<?php ////echo $id; ?>" readonly>
-                          </div>
-                           <div class="form-group">
-                           <label for="kategori"><b>Kategori</b></label>
-                            <select name="kategori" id="kategori" class="form-control" required oninvalid="this.setCustomValidity('Silahkan Pilih ')" oninput="setCustomValidity('')">
-                              <option value="">-Pilih-</option>
-                              <option value="Kategori 1">Kategori 1</option>
-                              <option value="Kategori 2">Kategori 1</option>
-                              <option value="Kategori 1">Kategori 1</option>
-                              <option value="Kategori 1">Kategori 1</option>
-                              <option value="Kategori 1">Kategori 1</option>
-                            </select>
-                          </div> 
-                          <div class="form-group">
-                            <label for="deskripsi"><b>Deskripsi</b></label>
-                            <textarea class="form-control" rows="5" name="deskripsi" id="deskripsi" placeholder="Deskripsi TroubleT"></textarea>
-                          </div>
-                          <div class="form-group">
-                            <label for="lampiran"><b>Lampiran</b></label>
-                            <input name="lampiran" type="file" class="form-control" placeholder="Lampiran" required oninvalid="this.setCustomValidity('Data tidak boleh kosong')" oninput="setCustomValidity('')" >
-                          </div>
-                  </div>
+                 <div class="form-group">
+                 <label for="kategori"><b>Kategori</b></label>
+                 <select name="id_kategori" class="form-control select2" data-placeholder="Pilih Nama Konversi" style="width: 100%;">
+                  <option value="id_kategori">===Pilih Nama Category===</option>
+                    <?php foreach($category as $key => $get) :?>
+                      <option value="<?php echo $get->id_kategori ?>"><?php echo $get->isi_kategori ?></option> 
+                    <?php endforeach; ?>
+                  </select>
+                  <p class="help-block"><?php echo form_error('id_kategori', '<small class="text-danger">', '</small>'); ?></p>
+                </div> 
+                <div class="form-group">
+                  <label for="deskripsi"><b>Deskripsi</b></label>
+                  <textarea class="form-control" rows="5" name="deskripsi" id="deskripsi" value="<?php echo set_value('deskripsi'); ?>" placeholder="Deskripsi TroubleT"></textarea>
                 </div>
-                <div class="panel-footer">
-                    <button type="submit" name="submit" class="btn btn-sm btn-primary"><span class="fa fa-save"></span> Simpan</button></div>
+                 <p class="help-block"><?php echo form_error('deskripsi', '<small class="text-danger">', '</small>'); ?></p>
+
+                <div class="form-group">
+                  <label for="lampiran"><b>Lampiran</b></label>
+                  <input name="file" type="file" class="form-control" placeholder="Lampiran">
+
                 </div>
-            </div>
-        </form>
+                <p class="help-block"><?php echo form_error('lampiran', '<small class="text-red">', '</small>'); ?></p>
+        </div>
+      </div>
+      <div class="panel-footer">
+          <button type="submit" class="btn btn-sm btn-primary"><span class="fa fa-save"></span> Simpan</button></div>
+      </div>
+  </div>
+</form>
         <script>
-$('#datepicker').datetimepicker({
-locale:'id',
-});
-</script>
+          $('#datepicker').datetimepicker({
+          locale:'id',
+          });
+        </script>
     </div>
   </div>
 <!--Add Modal -->
